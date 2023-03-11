12cf19210da9f3f719f472c09624cdfdbc71995b
emmanuel jonathan <emdo2841@gmail.com>
2023-03-11 08:38:36 -0800

    AirBnB

---
+++ one/models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a User class"""
+
+from models.base_model import BaseModel
+
+class City(Base_model):
+    """class for managing city object"""
+
+    state_id = ""
+    name = ""
